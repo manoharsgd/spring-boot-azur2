5e8a61bef4ada109386892e357622cdd504b07db
Manohar <mannusd12345@gmail.com>
2023-04-03 14:57:47 +0530

    first commit

---
+++ src/main/java/com/demo2/SpringBootAzur2Application.java
@@ -0,0 +1,13 @@
+package com.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzur2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzur2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo2/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.demo2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/demo")
+	public String getDemo()
+	{
+		return "Wel come to Sprong Boot Azur Demo2.....";
+	}
+
+}
